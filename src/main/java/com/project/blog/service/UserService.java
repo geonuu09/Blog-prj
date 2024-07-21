c28c15befbe630c88db36d3f1bc7fb7d6a971f1c
LeeGeonu <rjsdndl909@gmail.com>
2024-07-21 09:58:04 +0900

    OAUTH2 테스트 커밋

--- src/main/java/com/project/blog/service/UserService.java
+++ src/main/java/com/project/blog/service/UserService.java
@@ -15,12 +15,14 @@ import org.springframework.stereotype.Service;
 // 서비스 로직을 구현할 떄는 Entity에 직접 접근하기 보다는, 해당 로직에 맞는 dto를 새로 구성하여 그곳에 접근
 public class UserService {
     private final UserRepository userRepository;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public Long save(AddUserRequest dto) {
+
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
         return userRepository.save(User.builder()
                 .email(dto.getEmail())
-                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .password(encoder.encode(dto.getPassword()))
                 .build()).getId();
     }
 
